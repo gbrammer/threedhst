2bbcad084414398a89aa11c874881570012b8f5d
gbrammer <gbrammer@0125e826-dcd0-d3a4-0d96-e33e6880130e>
2010-07-06 15:41:54 +0000

    Added default options to __init__

--- threedhst/__init__.py
+++ threedhst/__init__.py
@@ -16,3 +16,34 @@ import shifts
 import sex
 import process_grism
 import plotting
+
+options = {}
+
+def defaultOptions():
+    """
+    defaultOptions()
+    
+    Set THREEDHST default options.
+    
+    To see the defaults, run
+    
+    >>> threedhst.defaultOptions()
+    >>> """
+    #### Delete all keywords and reset
+    for key in options.keys():
+        pop = options.popitem()
+        
+    options['DETECT_THRESH'] = 5.     ## Default 1.5
+    options['ANALYSIS_THRESH']  = 5.  ## Default 1.5
+    options['GRISM_NAME'] = 'G141'
+    
+defaultOptions()
+
+def printOptions():
+    """
+    printOptions()
+    
+    Show the current THREEDHST option set.
+    """
+    for key in options.keys():
+        print '%s = %s' %(key,str(options[key]))
